70d6ab4d95364927e28d7919d4cb3c3e24c5793f
jgpteam <dhanashrib@custom-soft.com>
2016-04-06 12:58:36 +0530

    Upload on Github 06 Apr 2016
    Solved issue of page redirect

--- JG_Prospect-New/JG_Prospect.web/Sr_App/ProductEstimate.aspx.cs
+++ JG_Prospect-New/JG_Prospect.web/Sr_App/ProductEstimate.aspx.cs
@@ -109,38 +109,39 @@ namespace JG_Prospect.Sr_App
                     Response.Redirect("Shutters.aspx?ProductTypeId=" + ddlproductlines.SelectedValue.Trim() + "&CustomerId=" + CustomerID);
                     break;
 
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "10":
-                case "11":
-                case "12":
-                case "13":
-                case "14":
-                case "15":
-                case "16":
-                case "17":
-                case "18":
-                case "19":
-                case "20":
-                case "21":
-                case "22":
-                case "23":
-                case "24":
-                case "25":
-                case "26":
-                case "27":
-                case "28":
-                case "29":
-                    Response.Redirect("Custom.aspx?ProductTypeId=" + ddlproductlines.SelectedValue.Trim() + "&CustomerId=" + CustomerID + "&Other=" + txtOther.Text.Trim());
-                    break;
+                //case "2":
+                //case "3":
+                //case "4":
+                //case "5":
+                //case "6":
+                //case "7":
+                //case "8":
+                //case "9":
+                //case "10":
+                //case "11":
+                //case "12":
+                //case "13":
+                //case "14":
+                //case "15":
+                //case "16":
+                //case "17":
+                //case "18":
+                //case "19":
+                //case "20":
+                //case "21":
+                //case "22":
+                //case "23":
+                //case "24":
+                //case "25":
+                //case "26":
+                //case "27":
+                //case "28":
+                //case "29":
+                //    Response.Redirect("Custom.aspx?ProductTypeId=" + ddlproductlines.SelectedValue.Trim() + "&CustomerId=" + CustomerID + "&Other=" + txtOther.Text.Trim());
+                //    break;
 
                 default:
+                    Response.Redirect("Custom.aspx?ProductTypeId=" + ddlproductlines.SelectedValue.Trim() + "&CustomerId=" + CustomerID + "&Other=" + txtOther.Text.Trim());
                     break;
             }
         }
